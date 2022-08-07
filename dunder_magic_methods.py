e19750d6ad6325753aa2daffd24e5f49cd48c1ab
Lalbahadur786 <guptalalbahadur@gmail.com>
2022-08-07 17:48:15 +0530

    third commit

--- dunder_magic_methods.py
+++ dunder_magic_methods.py
@@ -9,6 +9,29 @@ class Employee:
     def __init__(self, emp_list):
         print("I am in __init__ dunder method.")
         print(self.emp_list)
-
-emp1 = Employee(["e1","e2", "e3","e4"])
+    
+    def __add__(self, emp):
+        print("i am in __add__ dunder method")
+        return  ",".join(self.emp_list) + ", "+ emp
+    
+    def __getitem__(self, index):
+        print("I am in __getitem__ dunder method.")
+        return self.emp_list[index]
+    
+    def __setitem__(self, index, item):       
+        print("I am in __setitem__ dunder method.")
+        self.emp_list[index] = item
+    
+    def __contains__(self, item):
+        print ("I am in __contains__ dunder method")
+        return item in self.emp_list
+    
+    
+emp1 = Employee(["e1","e2", "e3","e4"])  # calls __new__()  first then __init__()
+#print(emp1 + "e7") calls __add__()
+#print(emp1[3])  calls __getitem__()
+#emp1[3] = "e5"  calls __setitem__()
+#print(emp1.emp_list)  prints all the elements on the emp_list
+#print("e4" in emp1) calls __contains__()
+#
 
